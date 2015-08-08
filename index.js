58384295fd47ff5e9d49f5bd2db732249409eded
Jonathan Lo <jlo@us.ibm.com>
2015-08-08 10:52:19 -0700

    working chat

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
