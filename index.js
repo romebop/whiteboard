f1d1a19f1a2c5276ce171dacca72c9d487ec2411
Jonathan Lo <jlo@us.ibm.com>
2015-08-08 13:44:19 -0700

    port 5000

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ io.on('connection', function(socket){
   })
 });
 
-http.listen(80, function(){
-  console.log('listening on *:80');
+http.listen(5000, function(){
+  console.log('listening on *:5000');
 });
