151e817dd488ac2c38157c380fdadb65306feda1
rome-bop <park.srome@gmail.com>
2015-08-08 15:39:36 -0700

    working socket.io tutorial

---
+++ assets/javascripts/main.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+
+    var socket = io();
+	
+	$('form').submit(function(){
+	  socket.emit('chat message', $('#m').val());
+	  $('#m').val('');
+	  return false;
+	});
+	
+	socket.on('chat message', function(msg){
+	  $('#messages').append($('<li>').text(msg));
+	});
+
+	console.log("i load");
+
+});
