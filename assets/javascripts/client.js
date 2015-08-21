5d8d2f22c18d06793d5ef7b413c1f822af7c20e0
rome-bop <park.srome@gmail.com>
2015-08-21 05:21:22 -0700

    local whiteboard works

--- assets/javascripts/client.js
+++ assets/javascripts/client.js
@@ -1,8 +1,75 @@
-$( document ).ready(function() {
+// source: http://stackoverflow.com/questions/2368784/draw-on-html5-canvas-using-a-mouse
 
-  var whiteboard =  document.getElementById("whiteboard");
-  var ctx = whiteboard.getContext('2d');
-  ctx.fillStyle = 'rgb(255,255,0)';
-  ctx.fillRect(0,0,600,480);
+var canvas, 
+  ctx, 
+  flag = false, 
+  prevX = 0, 
+  currX = 0, 
+  prevY = 0, 
+  currY = 0, 
+  dot_flag = false,
+  x = "black", 
+  y = 2;
 
-});
+function init() {
+    canvas = document.getElementById('whiteboard');
+    ctx = canvas.getContext("2d");
+
+    w = canvas.width;
+    h = canvas.height;
+    //console.log("width & height: " + w + " " + h);
+
+    canvas.addEventListener("mousemove", function (e) {
+        findxy('move', e)
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        findxy('down', e)
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        findxy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        findxy('out', e)
+    }, false);
+}
+
+function findxy(res, e) {
+    if (res == 'down') {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+
+        flag = true;
+        dot_flag = true;
+        if (dot_flag) {
+            ctx.beginPath();
+            ctx.fillStyle = x;
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
+        }
+    }
+    if (res == 'up' || res == "out") {
+        flag = false;
+    }
+    if (res == 'move') {
+        if (flag) {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+            draw();
+        }
+    }
+}
+
+function draw() {
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = x;
+    ctx.lineWidth = y;
+    ctx.stroke();
+    ctx.closePath();
+}
