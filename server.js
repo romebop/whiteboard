b1ec1e75d8ea99bc4ebf0db4dcdd3678dc1e3734
rome-bop <park.srome@gmail.com>
2015-09-03 15:45:23 -0700

    added comment to broadcast

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ io.on('connection', function(socket) {
   // receive a client emission, save canvas state, & emit to all clients
   socket.on('draw', function(params) {
     canvas_dataURL = params['canvas_dataURL'];
+    // emit to everyone except sender via broadcast
     socket.broadcast.emit('draw', params);
   });
 
