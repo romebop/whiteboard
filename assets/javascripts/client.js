9ca313af0a8186b7c52c749fc56679417e861952
rome-bop <park.srome@gmail.com>
2015-09-03 20:56:35 -0700

    cleaned up code

--- assets/javascripts/client.js
+++ assets/javascripts/client.js
@@ -4,30 +4,24 @@ var socket = io(),
   myId, 
   myHandle;
 
-// assign connection id
-socket.on('connection_id', function(connection_id) {
-  myId = connection_id;
-  myHandle = "Client " + myId;
-});
-
-// load global canvas and past 20 chats
 socket.on('load', function(params) {
+  // assign id and default handle
+  myId = params['connection_id'];
+  myHandle = "Client " + myId;
+  // load global canvas
   var stroke_history = params['stroke_history'];
   for (var stroke in stroke_history) {
-      draw_stroke(stroke_history[stroke]);
+    draw(stroke_history[stroke]);
   }
-
+  // load past 20 chats
   var chat_history = params['chat_history'];
-
   for (i = 0; i < chat_history.length; i++) {
-     write_chat( chat_history[i] );
+    write_chat( chat_history[i] );
   } 
-
 });
 
-// receive io emissions from server
 socket.on('draw', function(params) {
-  draw_stroke(params['stroke']);
+  draw(params['stroke']);
 });
 
 socket.on('clear', function() {
@@ -38,140 +32,86 @@ socket.on('chat message', function(msg_obj) {
   write_chat(msg_obj);
 });
 
-$(function() {
-  $("#h").focus();
-});
+/* whiteboard drawing behavior */
 
-$('#handleform').submit(function() {
-  if (!( $('#h').val() == '') ) {
-     myHandle = $('#h').val();
-  }
-  $('#handlebox').addClass('hide');
-  $('#chatbox').removeClass('hide');
-  $('#messages').scrollTop( $('#messages')[0].scrollHeight );
-  $(function() {
-    $("#m").focus();
-  });
-  return false;
-});
-
-$('#chatform').submit(function() {
-  socket.emit('chat message', { 'connection_id': myId, 'handle': myHandle, 'msg': $('#m').val() } );
-  $('#m').val('');
-  return false;
-});
-
-/* canvas drawing logic */
-// source: http://goo.gl/xxprq8
-
-// global variables
 var canvas,
   ctx,
   flag = false, 
   prevX = 0, 
-  currX = 0, 
   prevY = 0, 
+  currX = 0, 
   currY = 0, 
   dot_flag = false,
-  myColor = 'black', 
-  myWidth = 2;
+  myWidth = 2,
+  myColor = 'black'; 
 
 function init_canvas() {
-    canvas = document.getElementById('whiteboard');
-    ctx = canvas.getContext('2d');
+  canvas = document.getElementById('whiteboard');
+  ctx = canvas.getContext('2d');
+  // emit client input to server
+  canvas.addEventListener('mousemove', function (e) {
+    if (flag) {
+      update_xy(e);
+      draw([prevX, prevY, currX, currY, myWidth, myColor]);
+      emit_mouse('move', e);
+    }
+  }, false);
+  canvas.addEventListener('mousedown', function (e) {
+    flag = true;
+    update_xy(e);
+    draw([prevX, prevY, currX, currY, myWidth, myColor]);
+    emit_mouse('down', e);
+  }, false);
+  canvas.addEventListener('mouseup', function (e) {
+    flag = false;
+  }, false);
+  canvas.addEventListener('mouseout', function (e) {
+    flag = false;
+  }, false);
+}
 
-    // emit client input to server
-    canvas.addEventListener('mousemove', function (e) {
-	findxy('move', e.clientX, e.clientY, myColor, myWidth);
-        if (flag) emit_mouse('move', e);
-    }, false);
-    canvas.addEventListener('mousedown', function (e) {
-	flag = true;
-        findxy('down', e.clientX, e.clientY, myColor, myWidth);
-        emit_mouse('down', e);
-      }, false);
-    canvas.addEventListener('mouseup', function (e) {
-        findxy('up', e.clientX, e.clientY, myColor, myWidth);
-        emit_mouse('up', e);
-	flag = false;
-      }, false);
-    canvas.addEventListener('mouseout', function (e) {
-        findxy('out', e.clientX, e.clientY, myColor, myWidth);
-        emit_mouse('out', e);
-      }, false);
+function update_xy(e){
+  if (e.type == 'mousedown') {
+    prevX = e.clientX - canvas.offsetLeft;
+    prevY = e.clientY - canvas.offsetTop;
+    currX = e.clientX - canvas.offsetLeft;
+    currY = e.clientY - canvas.offsetTop;
+  } else if (e.type == 'mousemove') {
+    prevX = currX;
+    prevY = currY;
+    currX = e.clientX - canvas.offsetLeft;
+    currY = e.clientY - canvas.offsetTop;
+  }
 }
 
 function emit_mouse(type, e) {
   socket.emit( 'draw', { 'type': type, 'color': myColor, 'width': myWidth, 'id' : myId , 'canvasX' : e.clientX - canvas.offsetLeft , 'canvasY' : e.clientY - canvas.offsetTop} )
 }
 
-function findxy(res, clientX, clientY, color, width) {
-    if (res == 'down') {
-        prevX = currX;
-        prevY = currY;
-        currX = clientX - canvas.offsetLeft;
-        currY = clientY - canvas.offsetTop;
-        flag = true;
-        dot_flag = true;
-        if (dot_flag) {
-            ctx.beginPath();
-            ctx.fillStyle = color;
-            ctx.fillRect(currX, currY, 2, 2);
-            ctx.closePath();
-            dot_flag = false;
-        }
-    }
-    if (res == 'up' || res == 'out') {
-        flag = false;
-    }
-    if (res == 'move') {
-        if (flag) {
-            prevX = currX;
-            prevY = currY;
-            currX = clientX - canvas.offsetLeft;
-            currY = clientY - canvas.offsetTop;
-            draw(color, width);
-        }
-    }
-}
-
-
-function draw(color, width) {
-    ctx.beginPath();
-    ctx.moveTo(prevX, prevY);
-    ctx.lineTo(currX, currY);
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width;
+function draw(stroke) {
+  ctx.beginPath();
+  if ((stroke[0] == stroke[2]) && (stroke[1] == stroke[3])) {
+    ctx.fillStyle = stroke[5];
+    ctx.fillRect(stroke[0], stroke[1], 2, 2);
+  } else {
+    ctx.moveTo(stroke[0], stroke[1]);
+    ctx.lineTo(stroke[2], stroke[3]);
+    ctx.strokeStyle = stroke[5];
+    ctx.lineWidth = stroke[4];
     ctx.stroke();
-    ctx.closePath();
-}
-
-
-function draw_stroke(stroke) {
-    ctx.beginPath();
-    if ((stroke[0] == stroke[2]) && (stroke[1] == stroke[3])) {
-	ctx.fillStyle = stroke[5];
-        ctx.fillRect(stroke[0], stroke[1], 2, 2);
-    }
-    else {
-        ctx.moveTo(stroke[0], stroke[1]);
-        ctx.lineTo(stroke[2], stroke[3]);
-        ctx.strokeStyle = stroke[5];
-        ctx.lineWidth = stroke[4];
-        ctx.stroke();
-    }
-    ctx.closePath();
+  }
+  ctx.closePath();
 }
 
 function color(obj) {
-    myColor = obj.id;
-    if (myColor == 'white') {
-      myWidth = 14;
-      $('.indicator').attr('id', null);
-    } else {
-      myWidth = 2;
-      $('.indicator').attr('id', 'on' + myColor);
-    }
+  myColor = obj.id;
+  if (myColor == 'white') {
+    myWidth = 14;
+    $('.indicator').attr('id', null);
+  } else {
+    myWidth = 2;
+    $('.indicator').attr('id', 'on' + myColor);
+  }
 }
 
 function clear_canvas() {
@@ -188,6 +128,31 @@ function grid_mode() {
   }
 }
 
+/* chat messaging behavior */
+
+$(function() {
+  $("#h").focus();
+});
+
+$('#handleform').submit(function() {
+  if (!( $('#h').val() == '') ) {
+    myHandle = $('#h').val();
+  }
+  $('#handlebox').addClass('hide');
+  $('#chatbox').removeClass('hide');
+  $('#messages').scrollTop( $('#messages')[0].scrollHeight );
+  $(function() {
+    $("#m").focus();
+  });
+  return false;
+});
+
+$('#chatform').submit(function() {
+  socket.emit('chat message', { 'connection_id': myId, 'handle': myHandle, 'msg': $('#m').val() } );
+  $('#m').val('');
+  return false;
+});
+
 function write_chat(msg_obj) {
   var msg = msg_obj.sub_string_1 + display_time(msg_obj.date_ms) + msg_obj.sub_string_2;
   $('#messages').append($(msg));
@@ -195,29 +160,27 @@ function write_chat(msg_obj) {
 }
 
 function display_time(date_ms) {
-    var str = '';
-    var time = new Date(date_ms);
-    var hours = time.getHours();
-    var minutes = time.getMinutes();
-    var seconds = time.getSeconds();
-    var am_pm;
-    if (minutes < 10) {
-      minutes = '0' + minutes;
+  var time = new Date(date_ms);
+  var hours = time.getHours();
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
+  var am_pm;
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+  if (hours >= 12) {
+    am_pm = 'pm';
+    if (hours > 12) {
+      hours -= 12;
     }
-    if (seconds < 10) {
-      seconds = '0' + seconds;
+  } else {
+    am_pm = 'am';
+    if (hours == 0) {
+      hours = 12;
     }
-    if (hours >= 12) {
-      am_pm = 'pm';
-      if (hours > 12) {
-        hours -= 12;
-      }
-    } else {
-      am_pm = 'am';
-      if (hours == 0) {
-        hours = 12;
-      }
-    }
-    str += hours + ':' + minutes + am_pm; // + ':' + seconds + ' ';
-    return str;
+  }
+  return hours + ':' + minutes + am_pm; // + ':' + seconds + ' ';
 }
--- server.js
+++ server.js
@@ -1,18 +1,10 @@
+/* server config */
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var favicon = require('serve-favicon');
-
-var connection_id = 1;
-var last_messenger_id = -1;
-var chat_colors = ['white-msg','cloud-msg'];
-var current_chat_color = 0;
-var chat_history = [];     // used to store the last 20 messages
-var chat_history_current  = 0;   // used to organize the chathistory
-
-var stroke_history = [];
-var current_stroke = [];
 
 app.use(favicon(__dirname + '/assets/images/favicon.ico'));
 
@@ -27,45 +19,52 @@ http.listen(app.get('port'), function() {
   console.log('Server running on localhost:' + app.get('port'));
 });
 
-// per connection to client:
+/* server side socket */
+
+var connection_id = 1;
+var last_messenger_id = -1;
+var chat_colors = ['white-msg','cloud-msg'];
+var current_chat_color = 0;
+var chat_history = [];
+var chat_history_current = 0;
+
+var stroke_history = [];
+var current_stroke = [];
+
 io.on('connection', function(socket) {
   
   console.log("a connection has been made. id: " + connection_id);
-  // assign id to client
-  socket.emit('connection_id', connection_id);
+
+  // initialize client
+  socket.emit('load', { 'connection_id': connection_id, 'stroke_history' : stroke_history, 'chat_history' : chat_history });
+  connection_id++;
   // prevX, prevY, currX, currY, width, color
   current_stroke[connection_id] = [0,0,0,0,0,''];
-  connection_id++;
-
-  // emit canvas state for client to load
-  socket.emit('load', { 'stroke_history' : stroke_history, 'chat_history' : chat_history } );
 
   // receive a client emission, save canvas state, & emit to all clients
   socket.on('draw', function(params) {
-    var drawer_id  = params['id'];
-    var type       = params['type'];
-    var canvasX    = params['canvasX'];
-    var canvasY    = params['canvasY'];
-    var color      = params['color'];
-    var width      = params['width'];
+    var drawer_id = params['id'];
+    var type      = params['type'];
+    var canvasX   = params['canvasX'];
+    var canvasY   = params['canvasY'];
+    var width     = params['width'];
+    var color     = params['color'];
     if (type == 'down') {
-	current_stroke[drawer_id][0] = canvasX;
-	current_stroke[drawer_id][1] = canvasY;
-	current_stroke[drawer_id][2] = canvasX;
-	current_stroke[drawer_id][3] = canvasY;	
-        current_stroke[drawer_id][4] = width;
-        current_stroke[drawer_id][5] = color;
-	socket.broadcast.emit('draw', {'stroke' : current_stroke[drawer_id]});
+    	current_stroke[drawer_id][0] = canvasX;
+    	current_stroke[drawer_id][1] = canvasY;
+    	current_stroke[drawer_id][2] = canvasX;
+    	current_stroke[drawer_id][3] = canvasY;	
+      current_stroke[drawer_id][4] = width;
+      current_stroke[drawer_id][5] = color;
+	    socket.broadcast.emit('draw', {'stroke' : current_stroke[drawer_id]});
+    } else if (type == 'move') {
+    	current_stroke[drawer_id][2] = canvasX;
+    	current_stroke[drawer_id][3] = canvasY;
+    	socket.broadcast.emit('draw', {'stroke' : current_stroke[drawer_id]});
+    	stroke_history[stroke_history.length] = (current_stroke[drawer_id]).slice(0);
+    	current_stroke[drawer_id][0] = current_stroke[drawer_id][2];
+    	current_stroke[drawer_id][1] = current_stroke[drawer_id][3];
     }
-    if (type == 'move') {
-	current_stroke[drawer_id][2] = canvasX;
-	current_stroke[drawer_id][3] = canvasY;
-	socket.broadcast.emit('draw', {'stroke' : current_stroke[drawer_id]});
-	stroke_history[stroke_history.length] = (current_stroke[drawer_id]).slice(0);
-	current_stroke[drawer_id][0] = current_stroke[drawer_id][2];
-	current_stroke[drawer_id][1] = current_stroke[drawer_id][3];
-    }
-
   });
 
   socket.on('clear', function() {
@@ -74,27 +73,11 @@ io.on('connection', function(socket) {
   });
 
   socket.on('chat message', function(params) {
-    next_messenger_id = params['connection_id'];
-    var color;
+    var color = get_chat_color(params['connection_id']);
     var msg = params['msg'];
     var handle = params['handle'];
-    // Assign chat background color
-    // change chat color if next msg sender is different from last one
-    if (last_messenger_id != next_messenger_id) {
-        current_chat_color = (current_chat_color+1)%2;
-    }
-    last_messenger_id = next_messenger_id;
-    color = chat_colors[current_chat_color];	  
-    var sub_string_1 = '<li class="' + color + '"> <p id="time">[';
-    var date_ms = Date.now();
-    var sub_string_2 = ']</p> <b>' + handle + '</b>: ' + msg + '</li>';
-    // array of string + date object + string
-    var msg_obj = { 'sub_string_1': sub_string_1, 'date_ms': date_ms, 'sub_string_2': sub_string_2 }; 
-    // add to chat history
-    chat_history.push(msg_obj);
-    if (chat_history.length > 20) {
-       chat_history.shift();
-    }
+    var msg_obj = make_msg_obj(color, handle, msg);
+    update_chat_history(msg_obj);
     io.emit('chat message', msg_obj);
   });
 
@@ -103,3 +86,29 @@ io.on('connection', function(socket) {
   });
 
 });
+
+/* helper functions */
+
+function get_chat_color(next_messenger_id) {
+  // assign message color & change color if new sender
+  if (last_messenger_id != next_messenger_id) {
+    current_chat_color = (current_chat_color+1)%2;
+  }
+  last_messenger_id = next_messenger_id;
+  return chat_colors[current_chat_color];
+}
+
+function make_msg_obj(color, handle, msg) {
+  var sub_string_1 = '<li class="' + color + '"> <p id="time">[';
+  var date_ms = Date.now();
+  var sub_string_2 = ']</p> <b>' + handle + '</b>: ' + msg + '</li>';
+  // array of string + date object + string
+  return { 'sub_string_1': sub_string_1, 'date_ms': date_ms, 'sub_string_2': sub_string_2 };
+}
+
+function update_chat_history(msg_obj) {
+  chat_history.push(msg_obj);
+  if (chat_history.length > 20) {
+     chat_history.shift();
+  }
+}
