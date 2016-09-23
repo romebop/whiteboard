35575db68d9c40fd551a0887f72e7002463be4f1
rome-bop <park.srome@gmail.com>
2016-09-23 00:41:16 -0700

    fixed babel compile error

---
+++ server.es6.js
@@ -0,0 +1,118 @@
+var express    = require('express');
+var app        = express();
+var http       = require('http').Server(app);
+var io         = require('socket.io')(http);
+var favicon    = require('serve-favicon');
+var stringHash = require('string-hash');
+var async      = require('async');
+var bob        = require('./poop');
+
+var db         = require('./db');
+
+app.use(favicon(__dirname + '/assets/images/favicon.ico'));
+
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/assets'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+// connect to mongodb on start up
+db.connect(function(err) {
+  if (err) {
+    console.log('Unable to connect to Mongo.');
+    process.exit(1); // exit with failure code
+  } else {
+    http.listen(app.get('port'), function() {
+      console.log('Server running on localhost:' + app.get('port'));
+    });
+  }
+});
+
+/* server side socket */
+
+var connectionId = 1,
+  strokeById = {}, // necessary since canvas only has 'single cursor'
+  totalConnections = 0
+  ;
+
+io.on('connection', function(socket) {
+  
+  totalConnections++;
+  console.log('A connection has been made! ID: ' + connectionId);
+  socket.broadcast.emit('count', totalConnections);
+
+  // initialize client
+  async.parallel([
+    db.getStrokes,
+    db.getChats,
+  ], function(err, results) {
+    if (err) throw err;
+    var [strokes, chats] = results;
+    socket.emit('load', { 
+      'connectionId': connectionId,
+      'userCount': totalConnections, 
+      'strokeHistory': strokes, 
+      'chatHistory': chats,
+    });
+    strokeById[connectionId] = {};
+    connectionId++;
+  });
+
+  // receive client emission, save canvas state, & emit to all clients
+  socket.on('draw', function({ type, color, width, id, canvasX, canvasY }) {
+    var prevStroke = strokeById[id];
+    if (type === 'down') {
+      var currStroke = {
+        prevX: canvasX,
+        prevY: canvasY,
+        currX: canvasX,
+        currY: canvasY,
+        width,
+        color,
+      };
+    } else if (type === 'move') {
+      var currStroke = {
+        prevX: prevStroke.currX,
+        prevY: prevStroke.currY,
+        currX: canvasX,
+        currY: canvasY,
+        width,
+        color,
+      };
+    }
+    strokeById[id] = currStroke;
+    db.addStroke(currStroke);
+    socket.broadcast.emit('draw', {'stroke': currStroke});
+  });
+
+  socket.on('clear', function() {
+    db.clearStrokes();
+    io.emit('clear');
+  });
+
+  socket.on('chat', function({ handle, text }) {
+    var message = {
+      handle,
+      text,
+      color: getColor(handle),
+      date: Date.now(),
+    };
+    db.addChat(message);
+    io.emit('chat', message);
+  });
+
+  socket.on('disconnect', function() {
+    totalConnections--;
+    io.emit('count', totalConnections);
+    console.log('A user has disconnected.');
+  });
+
+});
+
+function getColor(handle) {
+  var colors = ['green', 'blue', 'red', 'black', 'orange'];
+  var randomIndex = Math.abs(stringHash(handle) % colors.length);
+  return colors[randomIndex];
+}
--- server.js
+++ server.js
@@ -1,67 +1,80 @@
-var express    = require('express');
-var app        = express();
-var http       = require('http').Server(app);
-var io         = require('socket.io')(http);
-var favicon    = require('serve-favicon');
-var stringHash = require('string-hash');
-var async      = require('async');
-var bob        = require('./poop');
+'use strict';
 
-var db         = require('./db');
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var favicon = require('serve-favicon');
+var stringHash = require('string-hash');
+var async = require('async');
+var bob = require('./poop');
+
+var db = require('./db');
 
 app.use(favicon(__dirname + '/assets/images/favicon.ico'));
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/assets'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 // connect to mongodb on start up
-db.connect(function(err) {
+db.connect(function (err) {
   if (err) {
     console.log('Unable to connect to Mongo.');
     process.exit(1); // exit with failure code
   } else {
-    http.listen(app.get('port'), function() {
-      console.log('Server running on localhost:' + app.get('port'));
-    });
-  }
+      http.listen(app.get('port'), function () {
+        console.log('Server running on localhost:' + app.get('port'));
+      });
+    }
 });
 
 /* server side socket */
 
 var connectionId = 1,
-  strokeById = {}, // necessary since canvas only has 'single cursor'
-  totalConnections = 0
-  ;
+    strokeById = {},
+    // necessary since canvas only has 'single cursor'
+totalConnections = 0;
 
-io.on('connection', function(socket) {
-  
+io.on('connection', function (socket) {
+
   totalConnections++;
   console.log('A connection has been made! ID: ' + connectionId);
   socket.broadcast.emit('count', totalConnections);
 
   // initialize client
-  async.parallel([
-    db.getStrokes,
-    db.getChats,
-  ], function(err, results) {
+  async.parallel([db.getStrokes, db.getChats], function (err, results) {
     if (err) throw err;
-    var [strokes, chats] = results;
-    socket.emit('load', { 
+
+    var _results = _slicedToArray(results, 2);
+
+    var strokes = _results[0];
+    var chats = _results[1];
+
+    socket.emit('load', {
       'connectionId': connectionId,
-      'userCount': totalConnections, 
-      'strokeHistory': strokes, 
-      'chatHistory': chats,
+      'userCount': totalConnections,
+      'strokeHistory': strokes,
+      'chatHistory': chats
     });
     strokeById[connectionId] = {};
     connectionId++;
   });
 
   // receive client emission, save canvas state, & emit to all clients
-  socket.on('draw', function({ type, color, width, id, canvasX, canvasY }) {
+  socket.on('draw', function (_ref) {
+    var type = _ref.type;
+    var color = _ref.color;
+    var width = _ref.width;
+    var id = _ref.id;
+    var canvasX = _ref.canvasX;
+    var canvasY = _ref.canvasY;
+
     var prevStroke = strokeById[id];
     if (type === 'down') {
       var currStroke = {
@@ -69,8 +82,8 @@ io.on('connection', function(socket) {
         prevY: canvasY,
         currX: canvasX,
         currY: canvasY,
-        width,
-        color,
+        width: width,
+        color: color
       };
     } else if (type === 'move') {
       var currStroke = {
@@ -78,37 +91,39 @@ io.on('connection', function(socket) {
         prevY: prevStroke.currY,
         currX: canvasX,
         currY: canvasY,
-        width,
-        color,
+        width: width,
+        color: color
       };
     }
     strokeById[id] = currStroke;
     db.addStroke(currStroke);
-    socket.broadcast.emit('draw', {'stroke': currStroke});
+    socket.broadcast.emit('draw', { 'stroke': currStroke });
   });
 
-  socket.on('clear', function() {
+  socket.on('clear', function () {
     db.clearStrokes();
     io.emit('clear');
   });
 
-  socket.on('chat', function({ handle, text }) {
+  socket.on('chat', function (_ref2) {
+    var handle = _ref2.handle;
+    var text = _ref2.text;
+
     var message = {
-      handle,
-      text,
+      handle: handle,
+      text: text,
       color: getColor(handle),
-      date: Date.now(),
+      date: Date.now()
     };
     db.addChat(message);
     io.emit('chat', message);
   });
 
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function () {
     totalConnections--;
     io.emit('count', totalConnections);
     console.log('A user has disconnected.');
   });
-
 });
 
 function getColor(handle) {
